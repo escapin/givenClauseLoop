54fdab2729474d6b55b9843ce201d01e3abd8f7a
springb0X <scapin.enrico@gmail.com>
2012-02-02 00:25:54 +0100

    all done

--- makeTest.sh
+++ makeTest.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 rm -rf $1_test.txt
-java -jar givenClauseLoopBatch.jar -o -contr -time300 $1 	>> $1_test.txt
+java -jar test/givenClauseLoopBatch.jar -o -contr -time300 $1 	>> $1_test.txt
 echo -n -e "\n##################################################################################\n\n" >> $1_test.txt
-java -jar givenClauseLoopBatch.jar -o -exp -time300 $1 		>> $1_test.txt
+java -jar test/givenClauseLoopBatch.jar -o -exp -time300 $1 		>> $1_test.txt
 echo -n -e "\n##################################################################################\n\n" >> $1_test.txt
-java -jar givenClauseLoopBatch.jar -e -contr -time300 $1		>> $1_test.txt
+java -jar test/givenClauseLoopBatch.jar -e -contr -time300 $1		>> $1_test.txt
 echo -n -e "\n##################################################################################\n\n" >> $1_test.txt
-java -jar givenClauseLoopBatch.jar -o -exp -time300 $1		>> $1_test.txt
+java -jar test/givenClauseLoopBatch.jar -o -exp -time300 $1		>> $1_test.txt
 echo -n -e "\n##################################################################################\n\n" >> $1_test.txt
